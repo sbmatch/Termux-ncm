1b6e1de91492404af3689ea491da0ca7e20eb7cb
3207754367 <3207754367@qq.com>
2019-04-21 14:47:39 +0800

    modified:   autodump.sh

--- autodump.sh
+++ autodump.sh
@@ -9,23 +9,49 @@ ps:ncmdump.py是在gayhub上偷的~
 **********************************"
 
 read -p "请回车安装依赖包..."
-pkg install wget && pkg upgrade -y && pkg update -y && apt-get install clang -y && pip install pycryptodome mutagen && pkg install -y python
-wget -N https://github.com/3207754367/ncmdump/raw/master/ncmdump.py
-echo "依赖包安装完成"
-read -p "请按回车键运行后台解密脚本:(定制ROM不要结束Termux哟~)"
+pkg install wget && pkg upgrade && pkg update  && pkg install  python && apt-get install clang -y && pip install --upgrade pip && pip install pycryptodome mutagen
 
+if [ -e ncmdump.py ]; then
+	echo "
+	ncmdump.py 已安装"
+else
+	wget -N https://github.com/3207754367/ncmdump/raw/master/ncmdump.py && chmod +x ncmdump.py
+	
+fi
+	echo "依赖包安装完成"
+	read -p "请按回车键运行后台解密脚本:(定制ROM不要结束Termux哟~)"
+
+echo "
+正在解密ing..."
 while :
 do
-if [ -e /sdcard/netease/cloudmusic/Music/*.ncm ]; then
+if [ -e "/sdcard/netease/cloudmusic/Music/*.ncm" ]; 
+then
 	#判断是否存在网易云ncm加密文件
 	mv /sdcard/netease/cloudmusic/Music/*.ncm .
 	#移动加密文件至解密脚本目录
-	python ncmdump.py &&  mv -f *.flac *.mp3 /sdcard/Music && break
-	#重定向解密文件至/sdcard/Music
-	echo $a 已解密,文件已输出到目录：/sdcard/Music
-	rm -rf *.ncm  #清理ncm加密文件
-else
-	echo "未找到ncm格式加密文件" >> /dev/null
+	python ncmdump.py && echo  "已解密,文件已输出到目录：/sdcard/Music" && rm -rf *.ncm  #重定向解密文件至/sdcard/Music/netease
 
+elif [ -e "/sdcard/Android/data/com.netease.cloudmusic/sdcard/netease/cloudmusic/music/*.ncm" ] ;
+then   
+	mv /sdcard/Android/data/com.netease.cloudmusic/sdcard/netease/cloudmusic/music/*.ncm . 
+	#移动加密文件至解密脚本目录
+        python ncmdump.py && echo "已解密,文件已输出到目录：/sdcard/Music/netease" && rm -rf *.ncm
+else
+	echo "没有找到ncm文件" >> /dev/null
 fi
+
+#输出解密后的文件
+a="*.mp3"
+b="*.flac"
+if [ -e "$a" ];then                                                           
+	 mv -f "$a" /sdcard/music/netease 
+	 #mp3 --> netease/
+ elif [ -f "$b" ];then
+	 mv -f "b" /sdcard/music/netease
+	 #flac --> netease/
+else
+echo "">/dev/null	 
+fi
+
 done
---
+++ ncmdump.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 15 01:05:58 2018
+
+@author: Nzix
+"""
+
+import binascii
+import struct
+import base64
+import json
+import os
+from Crypto.Cipher import AES
+from Crypto.Util.strxor import strxor as XOR
+from mutagen import mp3, flac, id3
+
+def dump(input_path, output_path = None, skip = True):
+
+    output_path = (lambda path, meta : os.path.splitext(path)[0] + '.' + meta['format']) if not output_path else output_path
+    core_key = binascii.a2b_hex('687A4852416D736F356B496E62617857')
+    meta_key = binascii.a2b_hex('2331346C6A6B5F215C5D2630553C2728')
+    unpad = lambda s : s[0:-(s[-1] if type(s[-1]) == int else ord(s[-1]))]
+
+    f = open(input_path, 'rb')
+
+    # magic header
+    header = f.read(8)
+    assert binascii.b2a_hex(header) == b'4354454e4644414d'
+
+    # key data
+    f.seek(2, 1)
+    key_length = f.read(4)
+    key_length = struct.unpack('<I', bytes(key_length))[0]
+
+    key_data = bytearray(f.read(key_length))
+    key_data = bytes(bytearray([byte ^ 0x64 for byte in key_data]))
+
+    cryptor = AES.new(core_key, AES.MODE_ECB)
+    key_data = unpad(cryptor.decrypt(key_data))[17:]
+    key_length = len(key_data)
+
+    # S-box (standard RC4 Key-scheduling algorithm)
+    key = bytearray(key_data)
+    S = bytearray(range(256))
+    j = 0
+
+    for i in range(256):
+        j = (j + S[i] + key[i % key_length]) & 0xFF
+        S[i], S[j] = S[j], S[i]
+
+    # meta data
+    meta_length = f.read(4)
+    meta_length = struct.unpack('<I', bytes(meta_length))[0]
+
+    meta_data = bytearray(f.read(meta_length))
+    meta_data = bytes(bytearray([byte ^ 0x63 for byte in meta_data]))
+    meta_data = base64.b64decode(meta_data[22:])
+
+    cryptor = AES.new(meta_key, AES.MODE_ECB)
+    meta_data = unpad(cryptor.decrypt(meta_data)).decode('utf-8')[6:]
+    meta_data = json.loads(meta_data)
+
+    # crc32
+    crc32 = f.read(4)
+    crc32 = struct.unpack('<I', bytes(crc32))[0]
+
+    # album cover
+    f.seek(5, 1)
+    image_size = f.read(4)
+    image_size = struct.unpack('<I', bytes(image_size))[0]
+    image_data = f.read(image_size)
+
+    # media data
+    output_path = output_path(input_path, meta_data)
+    if skip and os.path.exists(output_path): return
+
+    data = bytearray(f.read())
+    f.close()
+
+    # stream cipher (modified RC4 Pseudo-random generation algorithm)
+    # i = 0
+    # j = 0
+    # for k, _ in enumerate(data):
+    #     i = (i + 1) % 256
+    #     j = (i + S[i]) % 256 # in RC4, is j = (j + S[i]) % 256
+    #     # S[i], S[j] = S[j], S[i] # no swapping
+    #     data[k] ^= S[(S[i] + S[j]) % 256]
+
+    stream = [S[(S[i] + S[(i + S[i]) & 0xFF]) & 0xFF] for i in range(256)]
+    stream = bytearray(stream * (len(data) // 256 + 1))[1:1 + len(data)]
+    data = XOR(data, stream)
+
+    m = open(output_path, 'wb')
+    m.write(data)
+    m.close()
+
+    # media tag
+    if meta_data['format'] == 'flac':
+        audio = flac.FLAC(output_path)
+        # audio.delete()
+        image = flac.Picture()
+        image.encoding = 0
+        image.type = 3
+        image.mime = 'image/jpeg'
+        image.data = image_data
+        audio.clear_pictures()
+        audio.add_picture(image)
+    elif meta_data['format'] == 'mp3':
+        audio = mp3.MP3(output_path)
+        # audio.delete()
+        image = id3.APIC()
+        image.encoding = 0
+        image.type = 3
+        image.mime = 'image/jpeg'
+        image.data = image_data
+        audio.tags.add(image)
+        audio.save()
+        audio = mp3.EasyMP3(output_path)
+
+    audio['title'] = meta_data['musicName']
+    audio['album'] = meta_data['album']
+    audio['artist'] = '/'.join([artist[0] for artist in meta_data['artist']])
+    audio.save()
+    
+    return output_path
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        files = sys.argv[1:]
+    else:
+        files = [name for name in os.listdir('.') if os.path.splitext(name)[-1] == '.ncm']
+    
+    if sys.version[0] == '2':
+        files = [path.decode(sys.stdin.encoding) for path in files]
+
+    if not files:
+        print('please input file path!')
+        
+    for path in files:
+        try:
+            dump(path)
+            print(os.path.split(path)[-1])
+        except Exception as e:
+            print(e)
+            pass
